b5949f307eea27b08c2887a64148a5eb9324df35
Jason Fry <jason@feathr.co>
2017-03-29 17:41:01 -0400

    Add show route

--- assets/js/apps/contacts/contacts_app.js
+++ assets/js/apps/contacts/contacts_app.js
@@ -2,19 +2,29 @@ ContactManager.module("ContactsApp", function(ContactsApp, ContactManager,
 						Backbone, Marionette, $, _) {
 	ContactsApp.Router = Marionette.AppRouter.extend({
 		appRoutes: {
-			"contacts": "listContacts"
+			"contacts": "listContacts",
+			"contacts/:id": "showContact"
 		}
 	}); // end ContactsApp.Router
 
 	var API = {
 		listContacts: function() {
 			ContactsApp.List.Controller.listContacts();
+		},
+
+		showContact: function(id) {
+			ContactsApp.Show.Controller.showContact(id);
 		}
 	}; // end API
 
 	ContactManager.on("contacts:list", function() {
 		ContactManager.navigate("contacts");
 		API.listContacts();
+	});
+
+	ContactManager.on("contact:show", function(id) {
+		ContactManager.navigate("contacts/" + id);
+		API.showContact(id);
 	});
 
 	ContactsApp.on("start", function() {
--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -15,7 +15,7 @@ ContactManager.module("ContactsApp.List", function(
 
 			contactsListView.on("childview:contact:show",
 											function(childView, model) {
-				ContactManager.ContactsApp.Show.Controller.showContact(model);
+				ContactManager.trigger("contact:show", model.get("id"));
 			});
 
 			ContactManager.regions.main.show(contactsListView);
